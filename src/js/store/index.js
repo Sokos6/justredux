ad5769f2cf6a1a04443c19cfaad8430d0bb0241b
Sokos6 <will.sokolowski@gmail.com>
2019-12-25 21:22:59 -0600

    redux saga

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initialiseSagaMiddleware = createSagaMiddleware();
 
 const storeEnhaners = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhaners(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhaners(applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware))
 );
+
+initialiseSagaMiddleware.run(apiSaga);
 
 export default store;
